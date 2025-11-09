f44202251370c5aae2e20dc4dbb3005e14ae1d2d
intercaetera <author@intercaetera.com>
2025-11-09 15:17:28 +0100

    Blog list wip

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,16 +3,13 @@ import { glob } from 'astro/loaders';
 
 const blog = defineCollection({
 	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
+	loader: glob({ base: './src/content/articles', pattern: '**/*.{md,mdx}' }),
 	// Type-check frontmatter using a schema
-	schema: ({ image }) =>
+	schema: () =>
 		z.object({
 			title: z.string(),
-			description: z.string(),
-			// Transform string to Date object
 			pubDate: z.coerce.date(),
 			updatedDate: z.coerce.date().optional(),
-			heroImage: image().optional(),
 		}),
 });
 
